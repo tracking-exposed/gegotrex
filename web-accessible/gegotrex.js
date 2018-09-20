5d7bd913bc65d3cb1c13fcaed20e0e2d2c6396d1
Claudio Agosti <claudio@tracking.exposed>
2018-09-21 00:07:30 +0200

    addressed issue #1

--- web-accessible/gegotrex.js
+++ web-accessible/gegotrex.js
@@ -2,10 +2,12 @@ function initialize() {
 	console.log("initialize gegotrex.js");
 
     $("#expander").on('click', displayInfo);
-    $("#more").hide();
+    $("#more").on('click', displayInfo);
 
-//	$.getJSON('/api/v1/results/gegotrex', function(data) {
-	$.getJSON('/campaign/static.json', function(data) {
+	$.getJSON('/api/v1/results/gegotrex', function(data) {
+	$.getJSON('/campaign/SSL-results.json', function(ssl) {
+//	$.getJSON('/campaign/static.json', function(data) {
+
 
 		data = _.filter(data, function(o) {
 			return _.size(o.companies);
@@ -17,7 +19,22 @@ function initialize() {
         /* cleaning process */
         data = _.map(data, function(o) {
             o.href = o.href.replace(/^(http|https):\/\// ,'');
-            return o;
+            var match = _.find(ssl, { site: o.href});
+
+            if(!match) {
+                o.ssl = false;
+                return o;
+            }
+
+            o.description = match.description;
+            return _.extend(o, match.sslstatus);
+        });
+
+        var data = _.map(_.groupBy(data, 'description'), function(matches, name) {
+            // console.log(_.map(matches, 'test'));
+            var element = _.first(_.orderBy(matches, 'javascripts'));
+            element.sites = _.map(matches, 'href');
+            return element;
         });
 
 		var html = "";
@@ -25,15 +42,45 @@ function initialize() {
 			html += [
 				"<tr>", 
 					"<td>" + (i + 1) + "</td>", 
-					"<td>" + entry.href + "</td>",
-					"<td>" + _.uniq(entry.cookies).join(',') + "</td>",
-					"<td>" + entry.companies.join(',') + "</td>",
-					"<td>" + entry.unrecognized.join(',') + "</td>",
+					"<td>" + entry.description + "</br><small>(" + _.size(entry.sites)  + " sites)</small></td>",
+					"<td>" + 
+                        ( _.size(entry.cookies) 
+                            ?  '<span class="cookie">' + _.size(entry.cookies) + '</span> '
+                            : '' )
+                            + _.uniq(entry.cookies).join(',') + "</td>",
+					"<td>" + 
+					    ( _.size(entry.unrecognized) 
+                            ?  '<span class="company">' + _.size(entry.unrecognized) + '</span> '
+                            : '' )
+                            + entry.companies.join(',') + "</td>",
+					"<td>" +
+                        ( _.isString(entry.sslGrade) ? '<b>' + entry.sslGrade + '</b>' : '<i>error</i>' )
+                            + "</td>",
 				"</tr>"
 			].join("");
 		});
 		$("#results").append(html);
+
+        /* the stats */
+        var sslcount = _.countBy(data, function(entry) {
+            return (entry.test === "ok");
+        });
+
+        $("#stats--amount").text(_.size(data));
+        $("#stats--cookies").text(
+            _.reduce(data, function(memo, entry) {
+                memo += _.size(entry.cookies);
+                return memo;
+            }, 0)
+        );
+        $("#stats--companies").text(
+            _.size(_.uniq(_.flatten(_.map(data, 'companies'))))
+            + '/'
+            + _.size(_.flatten(_.map(data, 'companies')))
+        );
+        $("#stats--ssl").text(sslcount['true'] + '/' + sslcount['false']);
 	});
+    }); /* one get inside of the other, because the SSL is still a glued hack */
 }
 
 
